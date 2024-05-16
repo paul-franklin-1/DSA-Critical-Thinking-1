be1ca2b16948bd7e7a6aea45536eb749603abc21
Paul Franklin <paul.franklin@csuglobal.edu>
2024-05-16 14:01:45 -0400

    progress on main method in Driver class

---
+++ src/DriverClass.java
@@ -0,0 +1,10 @@
+public class DriverClass {
+    public static void main(String[] args) {
+        CandyBag JohnBag = new CandyBag();
+        JohnBag.addTreat("Twix");
+        JohnBag.addTreat("Milky Way");
+        for(int i=0;i<15;i++){
+            JohnBag.addTreat("Snickers");}
+
+
+}}
