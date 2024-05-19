146b0c87830929e7e3a2c8180f4c2b29d5f5e348
Paul Franklin <paul.franklin@csuglobal.edu>
2024-05-19 17:15:58 -0400

    entering javadoc comments, cleaning up some methods

--- src/Bag.java
+++ src/Bag.java
@@ -1,16 +1,39 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * this class constructs multisets that represent Halloween candy bags, and includes methods
+ * to interact with multiset instances
+ */
 public class Bag{
+    /**
+     * this attribute is a hashmap declaration that assists the constructor in creating
+     * an instance's multiset
+     */
     private Map<String,Integer> treats;
+
+    /**
+     * this is the constructor for the bag class that creates a new hashmap that is
+     * part of the new class object
+     */
     public Bag(){
         treats = new HashMap<>();
     }
+
+    /**
+     * this method adds a new element to the multiset
+     * @param brand
+     */
     public void add (String brand){
         if (treats == null) {
             treats = new HashMap<>();}
         treats.put(brand,treats.getOrDefault(brand,0)+1);
     }
+
+    /**
+     * this method prints a string representation of all the elements in the class object's,
+     * multiset, including all their keys and values
+     */
     public void print(){
         if(treats != null){
             System.out.println("\nCurrent contents of bag:\n[");
@@ -21,12 +44,20 @@ public class Bag{
             }System.out.println("]\n");}
         else{System.out.println("Alert: This bag does not exist");}}
 
-
+    /**
+     * @param brand
+     * @return the int value associated with the key entered by user as an argument
+     * If hashmap "treats" is null, method returns 0
+     */
     public int count(String brand) {
         if (treats != null){
             return treats.getOrDefault(brand, 0);}
         else{return 0;}}
 
+    /**
+     *
+     * @return
+     */
     public int size() {
         int totalTreats = 0;
         for(int count : treats.values()){
