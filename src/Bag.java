414833c6d9a415ae82a0b05f596d4a239599aafe
Paul Franklin <paul.franklin@csuglobal.edu>
2024-05-18 22:32:32 -0400

    exception handling implemented

--- src/Bag.java
+++ src/Bag.java
@@ -7,17 +7,25 @@ public class Bag{
         treats = new HashMap<>();
     }
     public void add (String brand){
+        if (treats == null) {
+            treats = new HashMap<>();}
         treats.put(brand,treats.getOrDefault(brand,0)+1);
     }
     public void print(){
-        System.out.println("\nCurrent contents of bag:\n[");
-        for (Map.Entry<String, Integer> entry : treats.entrySet()) {
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-            System.out.println("Key: " + key + ", Value: " + value);
-        }System.out.println("]");}
+        if(treats != null){
+            System.out.println("\nCurrent contents of bag:\n[");
+            for (Map.Entry<String, Integer> entry : treats.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                System.out.println("Key: " + key + ", Value: " + value);
+            }System.out.println("]\n");}
+        else{System.out.println("Alert: This bag does not exist");}}
+
+
     public int count(String brand) {
-        return treats.getOrDefault(brand, 0);}
+        if (treats != null){
+            return treats.getOrDefault(brand, 0);}
+        else{return 0;}}
 
     public int size() {
         int totalTreats = 0;
@@ -27,15 +35,17 @@ public class Bag{
 
     public void remove (String brand){
         int brandCount = treats.get(brand);
-        if (treats.containsKey(brand)){
+        if (brand != null && treats.containsKey(brand)){
             if(brandCount==1){
                 treats.remove(brand);}
             else{treats.put(brand,brandCount-1);}
         }
     }
     public void contains(String brand) {
-        boolean result = treats.containsKey(brand);
-        if (result) {
-            System.out.println("This bag contains the key \"" + brand + "\"");
-        } else {
-            System.out.println("This bag does not contain the key \"" + brand + "\"");}}}
+        if (treats != null){
+            boolean result = treats.containsKey(brand);
+            if (result) {
+                System.out.println("This bag contains the key \"" + brand + "\"");
+            } else {
+                System.out.println("This bag does not contain the key \"" + brand + "\"");}}
+        else{System.out.println("Alert: This bag does not exist");}}}
