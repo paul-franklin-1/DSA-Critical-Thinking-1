4d89c2dcd96606dddfc241cd4e7da5ddad1b1589
Paul Franklin <paul.franklin@csuglobal.edu>
2024-05-25 21:26:49 -0400

    excellent progress with new methods, successful runs in new main method

--- src/CandyBagManager2.java
+++ src/CandyBagManager2.java
@@ -1,24 +1,21 @@
 public class CandyBagManager2 {
     public static void main(String[] args) {
         Bag2<String> CandyBag2 = new Bag2<>();
+        Bag2<String> CandyBag3 = new Bag2<>();
         CandyBag2.add("Twix");
         CandyBag2.add("Twix");
         CandyBag2.add("Baby Ruth");
         for(int i=0;i<10;i++){
-            CandyBag2.add("Almond Joy");}
-        CandyBag2.add("Milky Way");
-        CandyBag2.add("Milky Way");
+            CandyBag3.add("Almond Joy");}
+        CandyBag3.add("Milky Way");
+        CandyBag3.add("Milky Way");
         for(int i=0;i<15;i++){
             CandyBag2.add("Snickers");}
+        System.out.println("Size Candy Bag #1: " + CandyBag2.size() + "\n" +
+                "Size Candy Bag #2: " + CandyBag3.size());
+        CandyBag2.merge(CandyBag3);
         CandyBag2.print(CandyBag2);
-        CandyBag2.contains("Milky Way");
-        CandyBag2.contains("Mars Bar");
-        System.out.println("Quantity of Milky Ways: " + CandyBag2.count("Milky Way"));
-        System.out.println("Quantity of Snickers: " + CandyBag2.count("Snickers"));
-        CandyBag2.remove("Milky Way");
-        CandyBag2.contains("Milky Way");
-        System.out.println("Quantity of Milky Ways: " + CandyBag2.count("Milky Way"));
-        CandyBag2.print(CandyBag2);
-        CandyBag2.size();
+        System.out.println("Distinct candy brands in bag: " + CandyBag2.distinct());
+
     }}
 
